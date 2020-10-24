c75a96adafa56461ac61403682471d2be3cfc0d8
Travis Norris <travisynorris@gmail.com>
2020-10-24 19:33:00 -0400

    Adding comment formatting

--- snip-gui/src/app/Components/comment-panel/comment-panel.component.ts
+++ snip-gui/src/app/Components/comment-panel/comment-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IComment } from 'src/app/models/models';
 
 @Component({
   selector: 'app-comment-panel',
@@ -11,6 +12,7 @@ export class CommentPanelComponent implements OnInit {
   //true -> user is actively creating a comment, this means that this is a modal dialogue
   //false -> comment is being displayed in the snippet view
   createMode: boolean;
+  @Input() comment: IComment;
 
   constructor() { }
 
--- snip-gui/src/app/models/stubs.ts
+++ snip-gui/src/app/models/stubs.ts
@@ -1,5 +1,5 @@
 import { LanguageTypeEnum } from './enums';
-import { IComment, ISnippet } from './models';
+import { IComment, IRegion, ISnippet } from './models';
 
 export class SnippetStub implements ISnippet
 {
@@ -15,11 +15,39 @@ export class SnippetStub implements ISnippet
     {
         this.id = "1234";
         this.path = "/api/v1/snippet/1234";
-        this.comments = []
+        this.comments = [
+            new CommentStub(),
+            new CommentStub(),
+            new CommentStub(),
+            new CommentStub(),
+            new CommentStub(),
+            new CommentStub(),
+            new CommentStub(),
+        ]
         this.info = "This is an example snippet";
         this.timestamp = Date().toLocaleLowerCase();
         this.language = LanguageTypeEnum.Java;
         this.content = "public static void main(String[] args) {\n\tSystem.out.println(\"Hello, World!\");\n}"
+    }
+    
+}
+
+export class CommentStub implements IComment
+{
+    id: string;
+    timestamp: string;
+    text: string;
+    region: IRegion;
+
+    constructor()
+    {
+        this.id = "1234";
+        this.timestamp = Date().toLocaleLowerCase();
+        this.text = "Nice job!\nLots of good stuff here";
+        this.region = {
+            start: 0,
+            end: 5
+        };
     }
     
 }
